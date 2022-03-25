a447779d25a009aeb4b50877dd83c9f3071effdf
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-03-25 15:03:03 -0500

    Add files via upload

--- Silent Auction/Silent Auction.py
+++ Silent Auction/Silent Auction.py
@@ -8,27 +8,53 @@ print(logo)
 print("Welcome to the Silent Auction")
 
 next_user = 1
-user_bids = []
+user_bids = {}
 
 def bidding(name_of_user, user_bid, other_user):
     current_input = {}
-    list_of_bids = {}
     current_input["name_of_user"] = name_of_user
     current_input["user_bid"] = user_bid
     current_input["other_user"] = other_user
-    list_of_bids[str(name)] = current_input
-    user_bids.append(list_of_bids)
+    user_bids[name] = current_input
+
 
 while next_user == 1:
     name = input("What is your name?\n")
     bid = float(input("What is your bid?\n"))
     other = input("Is anyone else bidding after you?\n").lower()
     bidding(name, bid, other)
+    clear()
     if other == "no":
         next_user -= 1
 
-print(user_bids)
+current_score = 0
 
-# TODO: compare user input values
-    # TODO: call the values from each entry in the dictionary
+for item in user_bids:
+    compare = user_bids[item]["user_bid"]
+    if compare > current_score:
+        current_score += compare
+
+for item in user_bids:
+    compare_two = user_bids[item]["user_bid"]
+    if compare_two == current_score:
+        print(f"The winner of the auction is {user_bids[item]['name_of_user']} with a bid of {user_bids[item]['user_bid']}. Congratulations!")
+
+
+
+
+# test = {
+#     "Nathan": ["Nathan", 20, "yes"],
+#     "Alison": ["Alison", 4, "yes"],
+#     "Nolan": ["Nolan", 6, "no"]
+# }
+
+# for item in test:
+#     test_item = test[item][1]
+#     print(test_item)
+
+# Nathan: {
+#     'name_of_user': 'Nathan', 
+#     'user_bid': 20.0,
+#     'other_user': 'yes',
+# }
 
